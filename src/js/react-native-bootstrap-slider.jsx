c945a31dd980d94e90771deb3bc3f53bde0e600c
Michael Brown <brownieboy@gmail.com>
2016-04-09 21:47:20 +1000

    Tidy up

--- src/js/react-native-bootstrap-slider.jsx
+++ src/js/react-native-bootstrap-slider.jsx
@@ -1,79 +1,9 @@
 /* eslint-env browser */
 
 import React from "react";
-// import ReactDOM from "react-dom";
-// import Slider from "bootstrap-slider";
 import browser from "detect-browser";
-// import es6BindAll from "es6bindall";
 import ReactNativeSlider from "react-html5-slider";
 import ReactBootstrapSlider from "react-bootstrap-slider";
-
-/* export class ReactNativeSlider extends React.Component {
-    render() {
-        return ( < input type = "range"
-            value = { this.props.value }
-            min = { this.props.min }
-            max = { this.props.max }
-            onInput = { this.props.handleChange }
-            onChange = { this.handleOnChange }
-            step = { this.props.step }
-            className = "react-native-slider" / >
-        );
-    }
-    handleOnChange() {
-        // Nothing to do here.  Only present to prevent reactjs warning
-        // about onChange not being present
-    }
-}
-
-export class ReactBootstrapSlider extends React.Component {
-    constructor(props) {
-        super(props);
-        es6BindAll(this, ["updateSliderValues"]);
-    }
-    render() {
-        // The slider"s an input.  That"s all we need.  We"ll do the rest in
-        // the componentDidMount() method.
-        return <input / >;
-    }
-
-    componentDidMount() {
-        var that = this;
-        this.mySlider = new Slider(ReactDOM.findDOMNode(this), {
-            "tooltip": this.props.tooltip || "show"
-        });
-        this.updateSliderValues();
-        this.mySlider.on("change", function(e) {
-            var fakeEvent = {
-                target: {}
-            };
-            fakeEvent.target.value = e.newValue;
-            that.props.handleChange(fakeEvent);
-        });
-    }
-    componentDidUpdate() {
-        this.updateSliderValues();
-    }
-    updateSliderValues() {
-        this.mySlider.setAttribute("min", this.props.min);
-        this.mySlider.setAttribute("max", this.props.max);
-        this.mySlider.setAttribute("step", this.props.step);
-        this.mySlider.setValue(this.props.value);
-
-        var sliderEnable = this.props.disabled === "disabled" ? false : true;
-        var currentlyEnabled = this.mySlider.isEnabled();
-        if (sliderEnable) {
-            if (!currentlyEnabled) {
-                this.mySlider.enable();
-            }
-        } else {
-            if (currentlyEnabled) {
-                this.mySlider.disable();
-            }
-        }
-    }
-} */
-
 
 export class ReactNativeBootstrapSlider extends React.Component {
     constructor(props) {
